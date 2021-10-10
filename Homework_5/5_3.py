30c4a439fadd4d6e69b658046178130453cfec2f
Kirill Peregud <kirill_peregud@mail.ru>
2021-10-10 20:45:42 +0300

    changing task#3

--- Homework_5/5_3.py
+++ Homework_5/5_3.py
@@ -1,14 +1,14 @@
 # task №5_3 by Kirill Peregud
 import random
-import math
 
 print('The program calculates the distance between points in N-dimensional space.')
 
-def validation_dim():
+def validation_dim(arg):
+    print('----------')
     num = 0
     while num not in range(1, 11):
         try:
-            num = int(input('\nEnter measure of dimensional:\n'))
+            num = int(input(f'\nEnter the size of the space for the {arg}:\n'))
         except:
             print('\nUnfortunately, the argument is not an integer :(\n')
         else:
@@ -16,81 +16,95 @@ def validation_dim():
                 break
             else:
                 print('\nPlease, enter a positive integer number from 1 to 10.')
+    print('----------\n')    
     return num
 
 def get_coordinates(n):
-    coord = [random.randint(0, 100) for _ in range(n)]
-    return tuple(coord)
+    coord = tuple([random.randint(0, 100) for _ in range(n)])
+    return coord
+
 
 class Point:
     
-    def __init__(self, n, coord_a):
-        self.n = n
-        self.coord_a = coord_a
+    def __init__(self, coords):
+        self.coords = coords
+
+    def __iter__(self):
+        self.val = 0
+        return self
+
+    def __next__(self):
+        if self.val < len(self.coords):
+            x = self.val
+            self.val += 1
+            return self.coords[x]
+        else:
+            raise StopIteration
+
+    def __abs__(self):
+        return sum([x ** 2 for x in self.coords]) ** 0.5
+
+    def __repr__(self):
+        text1 = f'Point coordinates -> {self.coords}\n'
+        text2 = 'Distance from point to origin -> %.2f\n' % abs(self)
+        return text1 + text2
 
 
-    def distance(self):
-        sum = 0
-        for x in self.coord_a:
-            sum += x ** 2
-        return math.sqrt(sum)
+class StraightLine:
 
+    def __init__(self, first_point, second_point):
+        self.point_a = Point(first_point)
+        self.point_b = Point(second_point)
 
-class StraightLine(Point):
-    def __init__(self, n, coord_a, coord_b):
-        super().__init__(n, coord_a)
-        self.coord_b = coord_b
+    def __abs__(self):
+        return sum(
+            [
+                (x - y) ** 2
+                for x, y in zip(
+                    self.point_a, self.point_b
+                )
+            ]
+        ) ** 0.5
 
-
-    def distance(self):
-        sum = 0
-        for x, y in zip(self.coord_a, self.coord_b):
-            sum += (x - y) ** 2
-        return math.sqrt(sum)
-
+    def __repr__(self):
+        text1 = f'Straight coordinates -> {self.point_a.coords}, {self.point_b.coords}\n'
+        text2 = 'Distance between points -> %.2f\n' % abs(self)
+        return text1 + text2
 
     def go_to_origin(self):
-        self.new_coord_a = []
-        self.new_coord_b = []
-        for x, y in zip(self.coord_a, self.coord_b):
+        self.new_point_a = []
+        self.new_point_b = []
+        for x, y in zip(self.point_a, self.point_b):
             delta = x
-            self.new_coord_a.append(x - delta)
-            self.new_coord_b.append(y - delta)
-        self.coord_a = tuple(self.new_coord_a)
-        self.coord_b = tuple(self.new_coord_b)
+            self.new_point_a.append(x - delta)
+            self.new_point_b.append(y - delta)
+        self.point_a.coords = tuple(self.new_point_a)
+        self.point_b.coords = tuple(self.new_point_b)
+
+        print(f'Straight new coordinates -> {self.point_a.coords}, {self.point_b.coords}')
+        print('Distance between new points -> %.2f\n' % abs(self))
 
 
-n = validation_dim()
+# Start of the program
+n_point = validation_dim('point')
 
-point_1 = Point(n, get_coordinates(n))
-point_2 = Point(n, get_coordinates(n))
+point_1 = Point(get_coordinates(n_point))
+print(point_1)
 
-print('----------\n')
-print(f'Point coordinates -> {point_1.coord_a}')
-print('Distance from point to origin -> %.2f\n' % point_1.distance())
-
-print(f'Point coordinates -> {point_2.coord_a}')
-print('Distance from point to origin -> %.2f\n' % point_2.distance())
-print('----------\n')
+point_2 = Point(get_coordinates(n_point))
+print(point_2)
 
 
-straight_1 = StraightLine(n, get_coordinates(n), get_coordinates(n))
-straight_2 = StraightLine(n, get_coordinates(n), get_coordinates(n))
+n_line = validation_dim('line')
 
-print(f'Straight coordinates -> {straight_1.coord_a}, {straight_1.coord_b}')
-print('Distance between points -> %.2f\n' % straight_1.distance())
+straight_1 = StraightLine(get_coordinates(n_line), get_coordinates(n_line))
+print(straight_1)
 
-print(f'Straight coordinates -> {straight_2.coord_a}, {straight_2.coord_b}')
-print('Distance between points -> %.2f\n' % straight_2.distance())
+straight_2 = StraightLine(get_coordinates(n_line), get_coordinates(n_line))
+print(straight_2)
 print('----------\n')
 
 
 straight_1.go_to_origin()
 straight_2.go_to_origin()
-
-print(f'Straight new coordinates -> {straight_1.coord_a}, {straight_1.coord_b}')
-print('Distance between new points -> %.2f\n' % straight_1.distance())
-
-print(f'Straight new coordinates -> {straight_2.coord_a}, {straight_2.coord_b}')
-print('Distance between new points -> %.2f\n' % straight_2.distance())
-print('----------')
+print('----------\n')
